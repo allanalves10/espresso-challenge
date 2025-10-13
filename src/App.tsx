38f3075e432fb1e27667247b0dc2866c300d77c9
Allan <alanjunior1997@gmail.com>
2025-10-12 21:03:49 -0300

    chore: add service config and interfaces

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,16 @@
+import { useEffect } from "react"
+import { api } from "./services/api"
+import type { IStatementResponse } from "./interfaces"
+
 function App() {
+  useEffect(() => {
+    (async () => {
+      const response = await api.get<IStatementResponse>("")
+      console.log(response)
+    }
+    )()
+  }, [])
+
   return (
     <>
       <h1>...</h1>
---
+++ src/interfaces/statement.interface.ts
@@ -0,0 +1,25 @@
+interface IMetadata {
+    count: number
+    page: number
+    limit: number
+    pages: number
+    last: number
+    prev: number | null
+    next: number | null
+}
+  
+interface IStatement {
+    id: string
+    description: string
+    amount: string
+    transaction_type: 'debit' | 'credit'
+    transaction_date: string
+    username: string
+    product_type: string
+}
+  
+export interface IStatementResponse {
+    metadata: IMetadata
+    data: IStatement[]
+}
+  

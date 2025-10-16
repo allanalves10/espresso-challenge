5f0286c75dac9cee1b5377509a968ffa0332e6ce
Allan <alanjunior1997@gmail.com>
2025-10-16 19:25:52 -0300

    fix: bold table header

--- src/atoms/TableCell/index.tsx
+++ src/atoms/TableCell/index.tsx
@@ -1,14 +1,15 @@
 import { TableCell } from '@mui/material'
-import type { TableCellProps } from '@mui/material'
+import type { SxProps, TableCellProps, Theme } from '@mui/material'
 import type { FC, ReactNode } from 'react'
 
 interface ITableCellCustomProps extends TableCellProps {
     children: ReactNode
+    sx?: SxProps<Theme>
 }
 
-export const TableCellCustom: FC<ITableCellCustomProps> = ({ children, ...props }) => {
+export const TableCellCustom: FC<ITableCellCustomProps> = ({ children, sx, ...props }) => {
     return (
-        <TableCell {...props} sx={{ fontSize: 14, padding: '8px 16px' }}>
+        <TableCell {...props} sx={{ fontSize: 14, padding: '8px 16px', ...sx }}>
             {children}
         </TableCell>
     )
